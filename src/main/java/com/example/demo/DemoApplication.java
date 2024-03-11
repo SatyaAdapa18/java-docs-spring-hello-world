2f203fe4decd43ef1323313b68ee0a247bd9580a
SatyaAdapa18 <134574521+SatyaAdapa18@users.noreply.github.com>
2024-03-12 10:23:35 +1100

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	String sayHello() {
-		return "Hello World, This is Satya from Melbourne !";
+		return "Hello World, This is Satya!";
 	}
 }
